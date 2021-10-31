0ad05effa4f62dcd4513f0603ce630483195a193
Ula Chin <ula.chin092@gmail.com>
2021-10-31 19:34:36 -0400

    oink

---
+++ src/redux/recipes.js
@@ -0,0 +1,54 @@
+export const recipes = 
+[
+    {
+        id: 0,
+        food: `Miso Noodle Veggie Soup`,
+        basicInfo: `1 Serving -- basic facts 
+        Calories: ~35 
+        Net carbs: ~1g 
+        Sodium: ~520mg 
+        Total fat: 1g saturated fat`,
+        ingredients: `Miko Brand Instant Miso Soup Awase-Variety-30% 
+        Coleslaw Mix 
+        Konjac Fettucine Noodle`,
+        benefits: `High fiber, low calories and so easy to prepare!`,
+        time: 'Prep time: no more than 10 minutes',
+        prep: `1. I'd recommend to boil water and rinse 1 pack of Shirataki fettuccine 
+        noodle thoroughly to get rid of the fishy smell. Don't reuse the fishy water with miso soup!
+        2. Use 4 ounces of coleslaw mix. Wash the coleslaw and drain.
+        3. Pour 6 oz of water into a pot with 1 serving of the miso soup base with the cole slaw until soup boils.`
+
+    },
+    {
+        id: 0,
+        food: `Peanut Sauce`,
+        basicInfo: `1 Serving basic facts: 
+        Calories: 35 
+        Net carbs: 4g carbs 
+        Sodium:  165 mg (265 with Sambal Olek or chilly garlic sauce)
+        Total fat: 1g saturated fat.`,
+        ingredients: `1 Tspn hoisin sauce by 365 Wholefoods brand 
+        1 tspn peanut powder 1 g carb 30 calories
+        (Optional: 1 tspn of Sambal Olek- ground chilli paste or chilli garlic sauce.)`,
+        benefits: `High fiber, low calories and so easy to prepare!`,
+        time: 'Prep time: no more than 1 minute',
+        prep: `1. Combine 1 tspn of hoisin sauce, 1 tspn of hoisin sauce, (option: 1 tspn of sambal or chilly 
+        garlic sauce) and mix until there is no dry ingredient. (Don't even need to cook! So easy!)
+        2. If it is too thick, add 1 tspn of water or how you like it.`
+
+    },
+    {
+    id: 0,
+    food: `Tortilla With Coleslaw Mix`,
+    basicInfo: `1 Serving Basic Facts:
+    Calories: 22 calories
+    Net Carbs: Less than a gram
+    Sodium: 130mg`,
+    ingredients: `1 ounce of coleslaw mix
+    1 La Banderita Carb Counter/0 carb Flour Tortilla`,
+    benefits: `High fiber, low calories, low sodium and so easy to prepare!`,
+    time: 'Prep time: no more than 5 minute',
+
+    
+    },
+]
